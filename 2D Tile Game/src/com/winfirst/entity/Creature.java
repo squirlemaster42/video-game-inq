3c38ce69dba9644c1c15789974cf22ec4396909a
squirlemaster42 <squirlemaster@gmail.com>
2015-12-06 12:10:39 -0500

    Changed player model

--- 2D Tile Game/src/com/winfirst/entity/Creature.java
+++ 2D Tile Game/src/com/winfirst/entity/Creature.java
@@ -4,8 +4,8 @@ public abstract class Creature extends Entity{
 
 	public static final int DEFAULT_HEALTH = 10;
 	public static final float DEFAULT_SPEED = 3.0f;
-	public static final int DEFAULT_CREATURE_WIDTH = 30;
-	public static final int DEFAULT_CREATURE_HEIGHT = 88;
+	public static final int DEFAULT_CREATURE_WIDTH = 32;
+	public static final int DEFAULT_CREATURE_HEIGHT = 100;
 	
 	protected int health;
 	protected float speed;
--- 2D Tile Game/src/com/winfirst/tile/Assets.java
+++ 2D Tile Game/src/com/winfirst/tile/Assets.java
@@ -12,13 +12,12 @@ public class Assets {
 	
 	public static void init(){
 		SpriteSheet ground = new SpriteSheet(ImageLoader.loadImage("/textures/ground.png"));
-		SpriteSheet character = new SpriteSheet(ImageLoader.loadImage("/textures/largerImage.png"));
 		SpriteSheet MainMenu = new SpriteSheet(ImageLoader.loadImage("/textures/MainMenu.png"));
+		SpriteSheet character = new SpriteSheet(ImageLoader.loadImage("/textures/Char.png"));
 		
 		grass = ground.crop(0, 0, width, height);
 		dirt = ground.crop(width, 0, width, height);
-		player = character.crop(0, 0, 15, 44);
-		tree = character.crop(25, 3, 34, 41);
+		player = character.crop(0, 0, 16, 50);
 		mainMenu = MainMenu.crop(0, 0, 640, 360);
 		rock = ground.crop(width * 2, 0, width, height);
 	}
