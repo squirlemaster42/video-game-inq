e2b47db118a972d7af91801decf1987ae356fa35
squirlemaster42 <squirlemaster@gmail.com>
2015-12-06 12:27:09 -0500

    Added worlds

--- 2D Tile Game/src/com/winfirst/states/GameState.java
+++ 2D Tile Game/src/com/winfirst/states/GameState.java
@@ -4,25 +4,29 @@ import java.awt.Graphics;
 
 import com.winfirst.entity.Player;
 import com.winfirst.game.Game;
-import com.winfirst.tile.Tile;
+import com.winfirst.world.World;
 
 public class GameState extends State{
 	
 	private Player player;
+	private World world;
 
 	
 	public GameState(Game game){
 		super(game);
 		player = new Player(game, 100, 100);
+		world = new World("");
 	}
 	
 	@Override
 	public void tick() {
+		world.tick();
 		player.tick();
 	}
 
 	@Override
 	public void render(Graphics g) {
+		world.render(g);
 		player.render(g);
 	}
 	
