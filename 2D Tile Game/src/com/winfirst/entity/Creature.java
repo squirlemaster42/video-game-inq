951411505fb3e89bc8a9844f8d119f5921a4a033
Jakob Misbach <squirlemaster@gmail.com>
2015-12-10 08:59:38 -0500

    Fixed collisions

--- 2D Tile Game/src/com/winfirst/entity/Creature.java
+++ 2D Tile Game/src/com/winfirst/entity/Creature.java
@@ -35,6 +35,8 @@ public abstract class Creature extends Entity{
 			if(!collisionWithTile(tx, (int)(y + bounds.y) / Tile.TILEHEIGHT) &&
 					!collisionWithTile(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT)){
 				x += xMove;
+			}else{
+				x = tx * Tile.TILEWIDTH - bounds.x - bounds.width - 1;
 			}
 			
 		}else if(xMove < 0){//Moving Left
@@ -43,6 +45,8 @@ public abstract class Creature extends Entity{
 			if(!collisionWithTile(tx, (int)(y + bounds.y) / Tile.TILEHEIGHT) &&
 					!collisionWithTile(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT)){
 				x += xMove;
+			}else{
+				x = tx * Tile.TILEWIDTH + Tile.TILEWIDTH - bounds.x;
 			}
 		}
 	}
@@ -54,13 +58,18 @@ public abstract class Creature extends Entity{
 			if(!collisionWithTile((int) (x + bounds.x) / Tile.TILEWIDTH, ty) &&
 					!collisionWithTile((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty)){
 				y += yMove;
-			}
+			}else{
+				y = ty * Tile.TILEHEIGHT + Tile.TILEHEIGHT - bounds.y;
+			}			
+			
 		}else if(yMove > 0){//Down
 			int ty = (int) (y + yMove + bounds.y + bounds.height) / Tile.TILEHEIGHT;
 			
 			if(!collisionWithTile((int) (x + bounds.x) / Tile.TILEWIDTH, ty) &&
 					!collisionWithTile((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty)){
 				y += yMove;
+			}else{
+				y = ty * Tile.TILEHEIGHT - bounds.y - bounds.height - 1;
 			}
 		}
 	}
