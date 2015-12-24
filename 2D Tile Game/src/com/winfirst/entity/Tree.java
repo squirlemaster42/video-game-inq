dd960b284596e3d3648b14e4ed9f047725ada58b
squirlemaster42 <squirlemaster@gmail.com>
2015-12-24 14:42:00 -0500

    Added tree bounding box

--- 2D Tile Game/src/com/winfirst/entity/Tree.java
+++ 2D Tile Game/src/com/winfirst/entity/Tree.java
@@ -10,6 +10,11 @@ public class Tree extends StaticEntity{
 
 	public Tree(Handler handler, float x, float y) {
 		super(handler, x, y, Tile.TILEWIDTH * 3, Tile.TILEHEIGHT * 3);
+		
+		bounds.x = 10;
+		bounds.y = (int) (height / 1.5f);
+		bounds.width = width - 20;
+		bounds.height = (int) (height - height / 1.5f);
 	}
 
 	@Override
