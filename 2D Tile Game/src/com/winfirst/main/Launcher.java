5f198e4f591bf8a883baff897466025f986f117c
squirlemaster42 <squirlemaster@gmail.com>
2016-01-09 22:59:23 -0500

    Launcher

---
+++ 2D Tile Game/src/com/winfirst/main/Launcher.java
@@ -0,0 +1,29 @@
+package com.winfirst.main;
+
+import com.winfirst.game.Game;
+import com.winfirst.settings.SettingsGUI;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		SettingsGUI settings = new SettingsGUI(300, 300);
+		settings.makeSettingsGui();
+		
+//		Game game = new Game("RPG Game", 720, 480);
+//		game.start();
+	}
+	
+	/* 
+	 * To do:
+	 * Finish Settings GUI and implement
+	 * 
+	 * Done:
+<<<<<<< HEAD
+=======
+	 * Complete Render Order
+	 * Fixed Tree Bounds
+	 * Added Settings GUI class
+	 * Added No Movement Animation
+>>>>>>> branch 'master' of https://github.com/squirlemaster42/RPG-Gaame
+	 */
+}
