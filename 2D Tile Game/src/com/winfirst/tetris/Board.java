8328def8393b538d66f7db5f7dee046c4f3e72ca
Jakob Misbach <jakobmisbach8@gmail.com>
2018-06-01 10:20:08 -0400

    Added logic to removed and moved lines
    When a line in completed, it will not be removed from the board.

--- 2D Tile Game/src/com/winfirst/tetris/Board.java
+++ 2D Tile Game/src/com/winfirst/tetris/Board.java
@@ -31,6 +31,8 @@ public class Board {
     private Random random;
 
     private Queue<Piece> pieceQueue;
+    
+    private Piece activePiece = null;
 
     public Board(Handler handler){
         this.handler = handler;
@@ -45,18 +47,63 @@ public class Board {
         pieceList.add(new Piece(handler, pieces[4], Color.CYAN));
         pieceList.add(new Piece(handler, pieces[5], Color.GREEN));
         pieceList.add(new Piece(handler, pieces[6], Color.MAGENTA));
-        
     }
 
     public void tick(){
+        ArrayList<Integer> removedLines = new ArrayList<>();
         if(pieceQueue.size() < 4) {
             pieceQueue.add(pieceList.get(random.nextInt(pieceList.size())));
         }
-
-        pieceList.forEach(Piece::tick);
+        
+        if(activePiece == null){
+            activePiece = pieceQueue.poll();
+        }
+        
+        if(!canMoveDown(activePiece){
+            activePiece == null;
+        }
+           
+        activePiece.tick();
+           
+        for(int i = 0; i < board.length; i++){
+            int sum = 0;
+            for(int j = 0; j < board[0].length; j++){
+                sum += board[i][j];
+            }
+            
+            if(sum == 10){
+                for(int j = 0; j < board[0].length; j++){
+                    board[i][j] = 0;
+                }
+                removedLines.add(i);
+            }
+        }
+           
+        for(int i = 0; i < removedLines.size(); i++){
+            for(j = removedLines[i]; j > 0; j--){
+                for(int k = 0; k < board[0].length; k++){
+                    board[j][k] = board[j - 1][k];
+                }
+            }
+        }
     }
 
     public void render(Graphics g){
         pieceList.forEach(e -> e.render(g));
+    }
+           
+    private boolean canMoveDown(){
+        return true;
+    }
+           
+    private int findRowWithFirstPiece(int[][] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[0].length; j++){
+                if(array[i][j] != 0){
+                    return i;
+                }
+            }
+        }
+        return 0;
     }
 }
