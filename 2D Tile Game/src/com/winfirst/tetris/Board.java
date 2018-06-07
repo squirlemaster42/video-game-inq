85e309e02197cefafc5a9cf44796ddfa78606925
Jakob Misbach <jakobmisbach8@gmail.com>
2018-06-07 14:49:26 -0400

    Updated print

--- 2D Tile Game/src/com/winfirst/tetris/Board.java
+++ 2D Tile Game/src/com/winfirst/tetris/Board.java
@@ -92,8 +92,14 @@ public class Board {
             activePiece = pieceQueue.poll();
         }
 
+        for(int i = 0; i < board.length; i++){
+            for(int j  = 0; j < board[0].length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
         System.out.println(activePiece.toString());
-        System.out.println(Arrays.deepToString(board));
 
         //Add piece to board
         assert activePiece != null;
