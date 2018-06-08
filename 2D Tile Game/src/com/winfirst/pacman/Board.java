4c63a980640b23ae6b6a667bb3c9cf3210b8c7b0
sdog2000 <39376813+sdog2000@users.noreply.github.com>
2018-06-08 17:35:47 -0400

    Added buildMapLst method
    - Added loops in buildMapLst in order to build the map's walls

--- 2D Tile Game/src/com/winfirst/pacman/Board.java
+++ 2D Tile Game/src/com/winfirst/pacman/Board.java
@@ -12,10 +12,54 @@ public class Board {
 	
 	public Board(Handler handler) {
 		this.handler = handler;
-		this.player = new Pacman(handler, 50.0f, 50.0f, 10, 10, 0, 0);
+		this.player = new Pacman(handler, 500.0f, 500.0f, 25, 25, 0, 0);
+		path = new int [30][27];
 		
 	}
 	
+	public int [][] buildMap(int[][] a) {
+		/*
+		 * Creates first row wall
+		 */
+		for (int wall1 = 0; wall1 <= a[0].length; wall1++) {
+			a[0][wall1] = 5;
+			}
+		/*
+		 * creates wall for first column
+		 */
+		for(int colwall = 0; colwall < 11; colwall++)
+			a[colwall][0] = 5;
+		for(int colwall = 20; colwall < a.length; colwall++)
+			a[colwall][0] = 5;
+		
+		/*
+		 * creates wall for last column
+		 */
+		
+		for(int colwall = 0; colwall < 11; colwall++)
+			a[colwall][26] = 5;
+		for(int colwall = 20; colwall < a.length; colwall++)
+			a[colwall][26] = 5;
+		/*
+		 * First Row
+		 */
+		for (int dotLine = 1; dotLine < 13; dotLine++) {
+			a[1][dotLine] = 1;
+		}
+		a[1][13] = 5;
+		for (int dotLine = 14; dotLine < 26; dotLine++) {
+			a[1][dotLine] = 1;
+		}
+		
+		/*
+		 * Last Rows
+		 */
+		for (int dotLine = 1; dotLine < 26; dotLine++)
+			a[31][dotLine] = 1;
+		for (int dotLine = 1; dotLine < a[30].length - 1; dotLine++)
+			a[30][dotLine] = 1;
+		return a;
+	}
 	public void tick() {
 		player.tick();
 	}
