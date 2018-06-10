1e690fd0161fa7300b999419e8ccdcd3b8d1ef69
sdog2000 <39376813+sdog2000@users.noreply.github.com>
2018-06-10 16:56:27 -0400

    Movement added to Pacman and Ghost entities
    Pacman can move with with WASD on keyboard, and ghosts chase Pacman

--- 2D Tile Game/src/com/winfirst/pacman/Ghost.java
+++ 2D Tile Game/src/com/winfirst/pacman/Ghost.java
@@ -8,19 +8,46 @@ import java.awt.*;
 public class Ghost extends Entity{
 	private int arrRow;
 	private int arrCol;
+	private Pacman player;
+
 		
-	public Ghost (Handler handler, float x, float y, int width, int height, int row, int col) {
+	public Ghost (Handler handler, float x, float y, int width, int height, int row, int col, Pacman player) {
 		super(handler, x, y, width, height);
 		this.arrRow = row;
 		this.arrCol = col;
+		this.player = player;
 	}
 			
 	public void tick() {
-	}
+		if (player.getX() < x)
+			x -= 2;
+		else if (player.getX() > x)
+			x += 2;
+		if (player.getY() < y)
+			y -= 2;
+		else if (player.getY() > y)
+			y += 2;
+		
+		/*if(checkEntityCollision(x, y, Pacman player)){
+            xMove ;
+        }
+
+        if(checkEntityCollision(x, y, Pacman player)){
+            yMove *= -1;
+        }
+
+		*/
+		
 	
+	
+	}
 	public void render(Graphics g) {
 	      g.setColor(Color.RED);
 	      g.fillRect((int) x, (int) y, width, height);
 
 	}
+	
+	
+
+	
 }
--- 2D Tile Game/src/com/winfirst/pacman/Pacman.java
+++ 2D Tile Game/src/com/winfirst/pacman/Pacman.java
@@ -8,7 +8,8 @@ import java.awt.*;
 public class Pacman extends Entity {
 	private int arrRow;
 	private int arrCol;
-	
+	private int xmove;
+	private int ymove;
 	public Pacman (Handler handler, float x, float y, int width, int height, int row, int col) {
 		super(handler, x, y, width, height);
 		this.arrRow = row;
@@ -16,11 +17,36 @@ public class Pacman extends Entity {
 	}
 		
 	public void tick() {
+		if (handler.getKeyManager().up) {
+			ymove = -3;
+			xmove = 0;
+		}
+		if (handler.getKeyManager().down) {
+			ymove = 3;
+			xmove = 0;
+		}
+		if (handler.getKeyManager().left) {
+			xmove = -3;
+			ymove = 0;
+		}
+		if (handler.getKeyManager().right) {
+			xmove = 3;
+			ymove = 0;
+		}
+		x += xmove;
+		y += ymove;
 	}
 	
 	public void render(Graphics g) {
         g.setColor(Color.YELLOW);
         g.fillRect((int) x, (int) y, width, height);
+	}
+	
+	public int getXmove() {
+		return xmove;
+	}
+	public int getYmove() {
+		return ymove; 
 	}
 
 }
