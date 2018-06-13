7a7fe7d494f13c2cca3d77558a4b6d363ad96c4b
sdog2000 <39376813+sdog2000@users.noreply.github.com>
2018-06-12 22:39:11 -0400

    Moved Over better Pacman Example doesn't work yet

---
+++ 2D Tile Game/src/com/winfirst/pacman/ThePacman.java
@@ -0,0 +1,16 @@
+package com.winfirst.pacman;
+import java.awt.Color;
+
+/** Written by Ryan D'souza
+  * Represents the Pacman */
+
+public class ThePacman extends PacmanItem {
+  public ThePacman(byte x, byte y, Color theColor) {
+    super(x, y, theColor);
+  }
+  
+  @Override
+  public String toString() { 
+    return "Pacman. X: " + this.x + "\tY: " + this.y;
+  }
+}
